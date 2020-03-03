509e31683197e7b55f816a18c23b8274cd2c05e1
Jeff Happily <goodboy96@live.com.my>
2020-03-03 12:27:18 +0800

    Finish parser

--- untyped/Parser.hs
+++ untyped/Parser.hs
@@ -6,12 +6,8 @@ import AST
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
-import Text.Parsec.Language (emptyDef, haskellStyle)
-
-import qualified Text.Parsec.Expr as Ex
+import Text.Parsec.Language (haskellStyle)
 import qualified Text.Parsec.Token as Tok
-
-import Data.Functor.Identity
 
 lexer :: Tok.TokenParser ()
 lexer = Tok.makeTokenParser style
@@ -24,17 +20,8 @@ lexer = Tok.makeTokenParser style
 parens :: Parser a -> Parser a
 parens = Tok.parens lexer
 
-reserved :: String -> Parser ()
-reserved = Tok.reserved lexer
-
-semiSep :: Parser a -> Parser [a]
-semiSep = Tok.semiSep lexer
-
 reservedOp :: String -> Parser ()
 reservedOp = Tok.reservedOp lexer
-
-prefixOp :: String -> (a -> a) -> Ex.Operator String () Identity a
-prefixOp s f = Ex.Prefix (reservedOp s >> return f)
 
 whiteSpace :: Parser ()
 whiteSpace = Tok.whiteSpace lexer
@@ -42,50 +29,20 @@ whiteSpace = Tok.whiteSpace lexer
 ident :: Parser String
 ident = Tok.identifier lexer
 
+variable :: Parser Expr
+variable = Var <$> Tok.identifier lexer
 
--- Prefix operators
-table :: Ex.OperatorTable String () Identity Expr
-table = [
-    [
-      -- prefixOp "let" Let
-    ]
-  ]
-
-var :: Parser Expr
-var = Var <$> Tok.identifier lexer
-
--- if/then/else
 lambda :: Parser Expr
--- lambda = do
---   reservedOp "\\"
---   whiteSpace
---   name <- many1 ident
---   reservedOp "->"
---   body <- expr
---   return foldr
-lambda = undefined
+lambda = do
+  reservedOp "\\"
+  whiteSpace
+  names <- many1 ident
+  reservedOp "->"
+  body <- expr
+  return $ foldr Lam body names
 
--- Constants
-true, false :: Parser Expr
-true  = reserved "true"  >> return (Lit (LBool True))
-false = reserved "false" >> return (Lit (LBool False))
--- zero  = reservedOp "0"   >> return Zero
-
-
-integerLiteral :: Parser Expr
-integerLiteral = (Lit . LInt . fromIntegral) <$> Tok.integer lexer
-
-expr :: Parser Expr
-expr = Ex.buildExpressionParser table factor
-
-factor :: Parser Expr
-factor =
-      true
-  <|> false
-  <|> integerLiteral
-  <|> parens expr
-  <|> lambda
-  <|> var
+number :: Parser Expr
+number = (Lit . LInt . fromIntegral) <$> Tok.integer lexer
 
 contents :: Parser a -> Parser a
 contents p = do
@@ -94,8 +51,16 @@ contents p = do
   eof
   return r
 
-toplevel :: Parser [Expr]
-toplevel = semiSep expr
+term :: Parser Expr
+term =  parens expr
+    <|> variable
+    <|> number
+    <|> lambda
+
+expr :: Parser Expr
+expr = do
+  es <- many1 term
+  return (foldl1 App es)
 
 parseExpr :: String -> Either ParseError Expr
 parseExpr s = parse (contents expr) "<stdin>" s
